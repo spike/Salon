69709a40d7e45af399bcd575234f53f9c6fd1b4c
Stephan <stephan@branczyk.org>
2022-09-14 21:05:22 -0700

    refined the list of services

--- app/src/main/java/com/github/spike/salon/salonservices/ServicesItem.kt
+++ app/src/main/java/com/github/spike/salon/salonservices/ServicesItem.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.spike.salon.R
@@ -17,15 +16,21 @@ import com.github.spike.salon.ServiceAvailable
 import com.github.spike.salon.ui.theme.SalonTheme
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun Cell(
     serviceListed: ServiceAvailable
 ) {
     Surface() {
         Card(
-            modifier = Modifier.padding(10.dp),
-            backgroundColor = Color(0xFF808080),
-            shape = RoundedCornerShape(8.dp)
+            modifier = Modifier.padding(10.dp)
+                .width(IntrinsicSize.Max),
+         //   backgroundColor = Color(0xFF808080),
+            shape = RoundedCornerShape(8.dp),
+            elevation = 2.dp,
+            onClick = {
+
+            }
         ) {
             Column(
                 modifier = Modifier.padding(8.dp),
@@ -33,7 +38,8 @@ fun Cell(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                 Card(
-                    modifier = Modifier.padding(8.dp),
+                    modifier = Modifier.padding(8.dp)
+                        .size(80.dp),
                     shape = CircleShape,
                 ) {
                     Icon(
@@ -46,8 +52,6 @@ fun Cell(
             }
         }
     }
-
-
 }
 
 
