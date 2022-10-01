b2e21ec7dc3b026f1bf33b9324ff36c03f5da698
Stephan <stephan@branczyk.org>
2022-10-01 15:24:42 -0700

    adding more previews to test on different device screens

--- app/src/main/java/com/github/spike/salon/ui/banner/Banner.kt
+++ app/src/main/java/com/github/spike/salon/ui/banner/Banner.kt
@@ -15,6 +15,9 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.github.spike.salon.ui.banner.Devices.NEXUS_5
+import com.github.spike.salon.ui.banner.Devices.PIXEL
+import com.github.spike.salon.ui.banner.Devices.PIXEL_3_XL
 import com.github.spike.salon.ui.theme.SalonTheme
 
 
@@ -134,7 +137,6 @@ fun CallToActionArrow() {
     }
 }
 
-//@ExperimentalFoundationApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultBannerPreview() {
@@ -144,3 +146,57 @@ fun DefaultBannerPreview() {
         }
     }
 }
+@Preview(showBackground = true, device = PIXEL_3_XL)
+@Composable
+fun DefaultBannerPreviewOnPixelXL() {
+    SalonTheme {
+        Column() {
+            Banner()
+        }
+    }
+}
+@Preview(showBackground = true, device = PIXEL)
+@Composable
+fun DefaultBannerPreviewOnPixel() {
+    SalonTheme {
+        Column() {
+            Banner()
+        }
+    }
+}
+@Preview(showBackground = true, device = NEXUS_5)
+@Composable
+fun DefaultBannerPreviewOnNexus5() {
+    SalonTheme {
+        Column() {
+            Banner()
+        }
+    }
+}
+
+object Devices {
+    const val DEFAULT = ""
+
+    const val NEXUS_7 = "id:Nexus 7"
+    const val NEXUS_7_2013 = "id:Nexus 7 2013"
+    const val NEXUS_5 = "id:Nexus 5"
+    const val NEXUS_6 = "id:Nexus 6"
+    const val NEXUS_9 = "id:Nexus 9"
+    const val NEXUS_10 = "name:Nexus 10"
+    const val NEXUS_5X = "id:Nexus 5X"
+    const val NEXUS_6P = "id:Nexus 6P"
+    const val PIXEL_C = "id:pixel_c"
+    const val PIXEL = "id:pixel"
+    const val PIXEL_XL = "id:pixel_xl"
+    const val PIXEL_2 = "id:pixel_2"
+    const val PIXEL_2_XL = "id:pixel_2_xl"
+    const val PIXEL_3 = "id:pixel_3"
+    const val PIXEL_3_XL = "id:pixel_3_xl"
+    const val PIXEL_3A = "id:pixel_3a"
+    const val PIXEL_3A_XL = "id:pixel_3a_xl"
+    const val PIXEL_4 = "id:pixel_4"
+    const val PIXEL_4_XL = "id:pixel_4_xl"
+
+    const val AUTOMOTIVE_1024p = "id:automotive_1024p_landscape"
+}
+
