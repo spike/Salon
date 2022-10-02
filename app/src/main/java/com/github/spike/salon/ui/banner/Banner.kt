248471b08ca6cc2096d2a13972fa112d85be6e42
Stephan <stephan@branczyk.org>
2022-10-01 19:30:30 -0700

    started fixing display

--- app/src/main/java/com/github/spike/salon/ui/banner/Banner.kt
+++ app/src/main/java/com/github/spike/salon/ui/banner/Banner.kt
@@ -13,13 +13,12 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_5
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_7
+import androidx.compose.ui.tooling.preview.Devices.PIXEL
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_3_XL
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.github.spike.salon.ui.banner.Devices.NEXUS_5
-import com.github.spike.salon.ui.banner.Devices.NEXUS_5X
-import com.github.spike.salon.ui.banner.Devices.NEXUS_7
-import com.github.spike.salon.ui.banner.Devices.PIXEL
-import com.github.spike.salon.ui.banner.Devices.PIXEL_3_XL
 import com.github.spike.salon.ui.theme.SalonTheme
 
 
@@ -44,40 +43,28 @@ fun Banner() {
         elevation = 2.dp,
         shape = RoundedCornerShape(16.dp),
         backgroundColor = Color(0xFFCBF5D7)
-
     ) {
-        DrawWhiteCircles()
         Column(
             modifier = Modifier.padding(8.dp),
-            verticalArrangement = Arrangement.Center,
+           // verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.End,
         ) {
-                Text(
-                    text = "Accumulate 20 points and get a free visit",
-                    modifier = Modifier.padding(
-                        start = 32.dp,
-                        end = 32.dp,
-                        top = 8.dp,
-                    ),
-                    style = MaterialTheme.typography.h6,
-                    fontWeight = FontWeight.SemiBold
-                )
-            Box () {
-
-                Card(
-                    modifier = Modifier
-                        .width(30.dp)
-                        .height(30.dp)
-                        .align(alignment = Alignment.CenterEnd)
-                    ,
-                    shape = CircleShape,
-                ) {
-
-                }
-                CallToActionArrow()
+            Text(
+                text = "Accumulate 20 points and\n get a free visit",
+                modifier = Modifier.padding(
+                    start = 32.dp,
+                    end = 32.dp,
+                    top = 8.dp,
+                ),
+                style = MaterialTheme.typography.h6,
+                fontWeight = FontWeight.SemiBold
+            )
+            Row(
+                modifier = Modifier,
+            ) {
+                DrawWhiteCircles()
+                DrawCircleArrow(Modifier)
             }
-
-
         }
     }
 }
@@ -86,53 +73,19 @@ fun Banner() {
 fun DrawWhiteCircles() {
     Canvas(
         modifier = Modifier
-            .size(size = 80.dp)
+            .size(size = 40.dp)
     ) {
         drawCircle(
             color = Color.White,
-            center = Offset(224f, 340f),
+            center = Offset(30.dp.toPx(), 180f),
             radius = 46.dp.toPx(),
             style = Stroke(width = 2.dp.toPx()),
         )
         drawCircle(
             color = Color.White,
-            center = Offset(320f, 358f),
+            center = Offset(0.dp.toPx(), 180.dp.toPx()),
             radius = 42.dp.toPx(),
             style = Stroke(width = 2.dp.toPx()),
-        )
-    }
-
-}
-@Composable
-fun CallToActionArrow() {
-    Canvas(modifier = Modifier
-        .width(90.dp)
-        .height(30.dp)
-
-    ) {
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-
-        drawLine(
-            start = Offset(x = canvasWidth-34f, y = canvasHeight/2),
-            end = Offset(x = 20f, y = canvasHeight/2),
-            color = Color.Black,
-            strokeWidth = 4f
-        )
-        drawLine(
-            start = Offset(x = canvasWidth-34f, y = canvasHeight/2),
-            end = Offset(x = 196f, y = canvasHeight/2+18),
-            color = Color.Black,
-            strokeWidth = 6f,
-            cap= StrokeCap.Round
-        )
-        drawLine(
-            start = Offset(x = canvasWidth-34f, y = canvasHeight/2),
-            end = Offset(x = 196f, y = canvasHeight/2-18),
-            color = Color.Black,
-            strokeWidth = 6f,
-            cap= StrokeCap.Round
-
         )
     }
 }
@@ -182,25 +135,25 @@ fun DefaultBannerPreviewOnNexus7() {
         }
     }
 }
-
+/*
 object Devices {
     const val DEFAULT = ""
 
-    const val NEXUS_7 = "id:Nexus 7"
+   // const val NEXUS_7 = "id:Nexus 7"
     const val NEXUS_7_2013 = "id:Nexus 7 2013"
-    const val NEXUS_5 = "id:Nexus 5"
+   // const val NEXUS_5 = "id:Nexus 5"
     const val NEXUS_6 = "id:Nexus 6"
     const val NEXUS_9 = "id:Nexus 9"
     const val NEXUS_10 = "name:Nexus 10"
     const val NEXUS_5X = "id:Nexus 5X"
     const val NEXUS_6P = "id:Nexus 6P"
     const val PIXEL_C = "id:pixel_c"
-    const val PIXEL = "id:pixel"
+   // const val PIXEL = "id:pixel"
     const val PIXEL_XL = "id:pixel_xl"
     const val PIXEL_2 = "id:pixel_2"
     const val PIXEL_2_XL = "id:pixel_2_xl"
     const val PIXEL_3 = "id:pixel_3"
-    const val PIXEL_3_XL = "id:pixel_3_xl"
+  //  const val PIXEL_3_XL = "id:pixel_3_xl"
     const val PIXEL_3A = "id:pixel_3a"
     const val PIXEL_3A_XL = "id:pixel_3a_xl"
     const val PIXEL_4 = "id:pixel_4"
@@ -208,4 +161,6 @@ object Devices {
 
     const val AUTOMOTIVE_1024p = "id:automotive_1024p_landscape"
 }
+
+ */
 
